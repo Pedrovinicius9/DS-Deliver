2004ed3495357210cffc8e38331637089b546a32
Pedro Vinicius <pedrooliveira0490@gmail.com>
2021-01-11 22:41:31 -0300

    Finish projct

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,19 +1,54 @@
-import React from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, ScrollView, Alert, Text } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { fetchOrders } from "../api";
 import Header from "../Header";
 import OrderCard from "../OrderCard";
+import { Order } from "../types";
 
 export default function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const handleOnPress = (order: Order) => {
+    navigation.navigate("OrderDetails", {
+      order,
+    });
+  };
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetchOrders()
+      .then((response) => setOrders(response.data))
+      .catch((error) => Alert.alert("Erro ao buscar os pedidos!!"))
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    if (isFocused) {
+      fetchData();
+    }
+  }, [isFocused]);
+
   return (
     <>
       <Header />
       <ScrollView style={styles.container}>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {isLoading ? (
+          <Text style={styles.title}>Buscando pedidos...</Text>
+        ) : (
+          orders.map((order) => (
+            <TouchableWithoutFeedback
+              key={order.id}
+              onPress={() => handleOnPress(order)}
+            >
+              <OrderCard order={order} />
+            </TouchableWithoutFeedback>
+          ))
+        )}
       </ScrollView>
     </>
   );
@@ -21,7 +56,15 @@ export default function Orders() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingRight: '5%',
-    paddingLeft: '5%'
-  }
+    paddingRight: "5%",
+    paddingLeft: "5%",
+  },
+  title: {
+    color: "#263238",
+    fontSize: 26,
+    lineHeight: 35,
+    fontWeight: "bold",
+    marginTop: 31,
+    textAlign: "center",
+  },
 });
